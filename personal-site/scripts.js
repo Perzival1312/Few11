2fc5bd489f35493a7ccbbd6aaea76b4b94dc5210
Perzival1312 <17786285+Perzival1312@users.noreply.github.com>
2023-04-22 07:15:26 -0400

    changes

--- personal-site/scripts.js
+++ personal-site/scripts.js
@@ -8,7 +8,9 @@ var sticky = header.offsetTop;
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+    header.classList.remove("nav-border");
   } else {
     header.classList.remove("sticky");
+    header.classList.add("nav-border");
   }
 }
